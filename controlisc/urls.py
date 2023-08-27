2428d763b41f46b19ee7f616e5352f95c3bbe1d2
ChinoISC <oscar010394@gmail.com>
2023-08-26 20:29:02 -0600

    static

--- controlisc/urls.py
+++ controlisc/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from api.Home.homeView import Home
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',Home.as_view(),name='index' )
 ]
+
+urlpatterns += staticfiles_urlpatterns()
